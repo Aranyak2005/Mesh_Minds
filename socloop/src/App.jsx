edb8377047eecb1be3cd543ee7aa3f7024a1837f
Aranyak2005 <aranyakghosh542@gmail.com>
2026-05-23 14:18:51 +0530

    Routes Created

--- socloop/src/App.jsx
+++ socloop/src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
+
+import Home from "./pages/Home";
+import Browse from "./pages/Browse";
+import Donate from "./pages/Donate";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+       
+        <Route path="/" element={<Home />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        
+        <Route path="/donate" element={<Donate />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
